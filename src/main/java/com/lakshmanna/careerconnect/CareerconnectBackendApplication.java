cb6a878b5349f1f90ae01d34b35f67ef311a1bfb
Lakshmanna <kuruvalakshmanna4154@gmail.com>
2026-07-21 16:57:04 +0530

    Initial project setup with Spring Boot and MySQL configuration

---
+++ src/main/java/com/lakshmanna/careerconnect/CareerconnectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lakshmanna.careerconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerconnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerconnectBackendApplication.class, args);
+	}
+
+}
